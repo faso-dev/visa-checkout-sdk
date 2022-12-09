4a6b03b79624e861159727fc54aa83e31fda8f55
faso-dev <jeromeonadja28@gmail.com>
2022-12-09 15:45:49 +0000

    feat(tests): add test for visa pay

---
+++ tests/VisaPayTest.php
@@ -0,0 +1,45 @@
+<?php
+	
+	namespace FasoDev\VisaCheckoutSdk\Tests;
+	
+	use FasoDev\VisaCheckoutSdk\CardData;
+	use FasoDev\VisaCheckoutSdk\Config;
+	use FasoDev\VisaCheckoutSdk\Credentials;
+	use FasoDev\VisaCheckoutSdk\VisaPay;
+	use PHPUnit\Framework\TestCase;
+	
+	class VisaPayTest extends TestCase
+	{
+		public function testVisaPay()
+		{
+			$visaCard = CardData::make(
+				'visa',
+				'4111111111111111',
+				'12',
+				'2025',
+				'123',
+				'John Doe',
+			);
+			
+			$config = Config::make(Credentials::make(
+				'VCO_USER_1234567890',
+				'VCO_PASS_1234567890',
+			));
+			
+			$payment = VisaPay::fromConfig($config);
+			
+			$this->assertEquals(
+				'https://sandbox.api.visa.com/checkout/v2/checkouts',
+				$config->checkoutUrl()
+			);
+			
+			$transaction = $payment->createPayment(
+				100.00,
+				'USD',
+				$visaCard,
+				'Payment description'
+			);
+			
+			$this->assertEquals('APPROVED', $transaction->status());
+		}
+	}
