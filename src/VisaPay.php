e7e0ab8fba9e0091a3ab341c65c1b9144ed13323
faso-dev <jeromeonadja28@gmail.com>
2022-12-09 15:42:58 +0000

    feat: implement payment for visa

---
+++ src/VisaPay.php
@@ -0,0 +1,52 @@
+<?php
+	
+	namespace FasoDev\VisaCheckoutSdk;
+	
+	class VisaPay implements PaymentInterface
+	{
+		use CheckoutRequestTrait;
+		
+		private Config $config;
+		
+		private function __construct(Config $config)
+		{
+			$this->config = $config;
+		}
+		
+		public static function fromConfig(Config $config): self
+		{
+			return new self($config);
+		}
+		
+		/**
+		 * @throws PaymentException
+		 */
+		public function createPayment(
+			float             $amount,
+			string            $currency,
+			CardDataInterface $cardData,
+			string            $description = null
+		): TransactionInterface
+		{
+			$response = $this->request(
+				'POST',
+				$this->config->checkoutUrl(),
+				$this->requestBody($amount, $currency, $cardData, $description),
+			);
+			
+			if ($response->status() !== 201) {
+				throw new PaymentException(
+					$response->json()['responseStatus']['message'],
+					$response->status(),
+				);
+			}
+			
+			return $this->handleResponse(
+				$this->request(
+					'POST',
+					$this->config->checkoutUrl(),
+					$this->requestBody($amount, $currency, $cardData, $description),
+				)->json(),
+			);
+		}
+	}
